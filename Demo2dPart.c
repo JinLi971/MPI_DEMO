e503f5e2fe2d6fbfc3b733bfc9663a15326d5129
Jing Liu <jing.liu@it.uu.se>
2015-02-03 15:50:33 +0100

    2d data partitioning

--- Demo2dPart.c
+++ Demo2dPart.c
@@ -12,8 +12,8 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    const int NPROWS=2;  /* number of rows in _decomposition_ */
-    const int NPCOLS=4;  /* number of cols in _decomposition_ */
+    const int NPROWS=4;  /* number of rows in _decomposition_ */
+    const int NPCOLS=2;  /* number of cols in _decomposition_ */
     const int BLOCKROWS = ROWS/NPROWS;  /* number of rows in _block_ */
     const int BLOCKCOLS = COLS/NPCOLS; /* number of cols in _block_ */
     double *sendbuf = NULL;
@@ -25,6 +25,8 @@ int main(int argc, char **argv) {
 
 
     if (rank == 0) {
+        printf("global matrix is %d*%d, and sub matrix is %d*%d\n", ROWS,COLS,BLOCKROWS   ,BLOCKCOLS);
+        printf("grid is %d by %d \n", NPROWS,NPCOLS);
         sendbuf =  (double*) malloc(ROWS*COLS*sizeof(double));
         //set send buffer to a random number
         for (int ii=0; ii<ROWS*COLS; ii++) {
@@ -46,11 +48,10 @@ int main(int argc, char **argv) {
         }
     }
 
-    MPI_Scatterv(sendbuf, counts, disps, Pdatatype, recvbuf, BLOCKROWS*BLOCKCOLS, MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(sendbuf, counts, disps, Pdatatype, recvbuf, BLOCKROWS*BLOCKCOLS, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     /* each proc prints it's "b" out, in order */
     for (int proc=0; proc<p; proc++) {
         if (proc == rank) {
-            printf("Rank = %d\n", rank);
             if (rank == 0) {
                 printf("Global matrix: \n");
                 for (int ii=0; ii<ROWS; ii++) {
@@ -60,7 +61,7 @@ int main(int argc, char **argv) {
                     printf("\n");
                 }
             }
-            printf("Local Matrix:\n");
+            printf("Local Matrix: at rank %d\n",rank);
             for (int ii=0; ii<BLOCKROWS; ii++) {
                 for (int jj=0; jj<BLOCKCOLS; jj++) {
                     printf("%.3f ",recvbuf[ii*BLOCKCOLS+jj]);
@@ -71,6 +72,37 @@ int main(int argc, char **argv) {
         }
         MPI_Barrier(MPI_COMM_WORLD);
     }
+
+
+
+
+    //for shifting Isend and Irecv
+    double *recvbuf2 =(double*) malloc(sizeof(double)*(BLOCKROWS*BLOCKCOLS));
+     int right = (rank + 1) % p;
+     int left = rank - 1;
+     if (left < 0)
+         left = p - 1;
+     MPI_Request request, request2;
+     MPI_Status status;
+
+     MPI_Irecv(recvbuf2, BLOCKROWS*BLOCKCOLS, MPI_DOUBLE , left, 123, MPI_COMM_WORLD, &request);
+     MPI_Isend(recvbuf,  BLOCKROWS*BLOCKCOLS, MPI_DOUBLE , right, 123, MPI_COMM_WORLD, &request2);
+     MPI_Wait(&request, &status);
+
+     for (int proc=0; proc<p; proc++) {
+         if (proc == rank) {
+             printf("Rank = %d\n", rank);
+             printf("Local Matrix:\n");
+             for (int ii=0; ii<BLOCKROWS; ii++) {
+                 for (int jj=0; jj<BLOCKCOLS; jj++) {
+                     printf("%.3f ",recvbuf2[ii*BLOCKCOLS+jj]);
+                 }
+                 printf("\n");
+             }
+             printf("\n");
+         }
+         MPI_Barrier(MPI_COMM_WORLD);
+     }
 
     MPI_Finalize();
     return 0;
